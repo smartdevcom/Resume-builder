cec9fc0a2895874969bf70c3d43513244a5a9b8d
simba <simba.fan@outlook.com>
2019-09-06 12:52:25 +1000

    app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import css from root
 import './index.css';
+//import app for run
 import App from './containers/App';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
